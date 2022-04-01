1293eccdf6c33fafd413410dbb0a1039a78e9b09
wilricknl <83393813+wilricknl@users.noreply.github.com>
2022-04-01 14:24:49 +0200

    Add Windows example

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "webview.h"
+
+#include <filesystem>
+
+int main() {
+    webview::webview w(true, nullptr);
+    w.set_title("Minimal example");
+    w.set_size(480, 320, WEBVIEW_HINT_NONE);
+    w.navigate(R"(file://)" + std::filesystem::current_path().string() + "/index.html");
+    w.run();
+    return 0;
+}
