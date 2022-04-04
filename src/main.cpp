2dcd270e5ed19e2ca6df1425d62a7567c55b3c89
wilricknl <83393813+wilricknl@users.noreply.github.com>
2022-04-04 14:45:11 +0200

    Add sample GUI

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
     webview::webview w(true, nullptr);
     w.set_title("Minimal example");
     w.set_size(480, 320, WEBVIEW_HINT_NONE);
-    w.navigate(R"(file://)" + std::filesystem::current_path().string() + "/index.html");
+    w.navigate(R"(file://)" + std::filesystem::current_path().string() + "/gui/index.html");
     w.run();
     return 0;
 }
